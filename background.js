fb688b4f3d8f56eb593716847f5f5344fc3367dc
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2022-02-25 15:06:55 +0000

    Adding ability to combine metadata and content tiers

--- background.js
+++ background.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     var btn = document.getElementById('submit');
     btn.addEventListener('click', function() {
-        var url;
-        var string;
-        var tier = document.getElementById('tier').value;
-        if (tier[0] == "M") {
-            qstring = "&qf=metadataTier:"+tier[tier.length - 1];
-        } else {
-            qstring = "&qf=contentTier:"+tier[tier.length - 1]
+        var qstring = "";
+        var mtier = document.getElementById('metadata').value;
+        if (mtier != "-") {
+            qstring += "&qf=metadataTier:"+mtier[mtier.length - 1];
+        }            
+        var ctier = document.getElementById('content').value
+        if (ctier != "-") {
+            qstring += "&qf=contentTier:"+ctier[ctier.length - 1];
         }
+
         chrome.tabs.query({ active: true, currentWindow: true}, function(tabs) {
             let url = tabs[0].url
             if (url.includes("europeana.eu") || url.includes("metis")) {
-                url = url.replace(/&qf=(metadata|content)Tier%3A./, "");
+                url = url.replace(/&qf=metadataTier%3A./, "");
+                url = url.replace(/&qf=contentTier%3A./, "");
                 let newUrl = url+qstring
                 chrome.tabs.update(undefined, { url: newUrl });
             }
